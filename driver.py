71882b0782ddae98ebab11f7e772ddb510f07730
Saurabh Agrawal <saurabhagrawal0412@gmail.com>
2018-04-25 20:38:59 -0400

    Adding generalized logic for prediction

--- driver.py
+++ driver.py
@@ -9,10 +9,9 @@ from configparser import ConfigParser
 import sys
 
 from data_reader import get_ratings_df
-import dimensionality_reduction
 from neighbor_selection import select_neighbors
 from rating_normalization import perform_normalization
-import prediction
+from prediction import predict
 from similarity_computation import compute_similarity
 from validation import compute_validation_error
 
@@ -24,14 +23,21 @@ def main():
     parser.add_argument('config_file', help='Location of the config file')
     config = ConfigParser()
     config.read(parser.parse_args().config_file)
+
     train_ratings_df, test_ratings_df = get_ratings_df(config)
     train_ratings_df = perform_normalization(config, train_ratings_df)
-    similarity_df = compute_similarity(config, train_ratings_df)
-    nearest_dict = select_neighbors(config, similarity_df)
 
-    # predicted_ratings = dimensionality_reduction.predict(config, ratings_df)
-    # print predicted_ratings.view()
-    prediction_df = prediction.get_avg_rating_recc(train_ratings_df, nearest_dict)
+    similarity_df, nearest_dict = None, None
+    # similarity_df = compute_similarity(config, train_ratings_df)
+    # nearest_dict = select_neighbors(config, similarity_df)
+    # prediction_df = prediction.get_avg_rating_recc(train_ratings_df, nearest_dict)
+
+    prediction_df = predict(config, train_ratings_df, similarity_df, nearest_dict)
+    print type(prediction_df)
+    from data_reader import display_dataframe
+    display_dataframe(train_ratings_df, 'training.html')
+    display_dataframe(prediction_df, 'svd_pred.html')
+
     validation_error = compute_validation_error(config, test_ratings_df, prediction_df)
     print 'Validation error ->', validation_error
 
